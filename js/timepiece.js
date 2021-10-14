55a0263877bcced2032d4775dc75cad2999f6866
hujiangtao <1936787441@qq.com>
2021-10-14 17:59:01 +0800

    Site updated: 2021-10-14 17:58:59

--- js/timepiece.js
+++ js/timepiece.js
@@ -1,4 +1,5 @@
 let instance = null;
+let timer = null;
 class Timepiece {
 
     constructor() {
@@ -12,13 +13,14 @@ class Timepiece {
     els = null;
 
     init() {
+        if(timer) return timer;
         console.log('init timepiece...');
         const timeDom = $('<ul><hr/></ul>');
         timeDom[0].classList.add('clock');
         timeDom[0].setAttribute('id', 'helang-clock');
         timeDom.appendTo('#page-header');
         this.$el = timeDom;
-        
+        timer = this.$el;
         let nowDate=new Date();
             this.dateInfo={
                 "year":nowDate.getFullYear(),
@@ -267,10 +269,87 @@ class MyEvents {
 
     init() {
         console.log('init user events');
+        this.checkPageSize();
+        $(window).resize(() =>{
+            //执行代码块
+            this.checkPageSize();
+         });
+    }
+    checkPageSize() {
+        if($(window).width() <= 720) {
+            if(timer){
+                timer.remove();
+                timer = null;
+            }
+            
+            $('#page-header').attr('page-size','min');
+        }else{
+            new Timepiece();
+            $('#page-header').attr('page-size', 'normal' );
+        }
     }
 }
+
+// pwa相关
+class PWA {
+    constructor() {
+        this.insertDom();
+        if ('serviceWorker' in navigator) {
+            if (navigator.serviceWorker.controller) {
+              navigator.serviceWorker.addEventListener('controllerchange', function () {
+                showNotification()
+              })
+            }
+        
+            window.addEventListener('load', function () {
+              navigator.serviceWorker.register('/sw.js')
+            })
+          }
+        
+          function showNotification() {
+            if (GLOBAL_CONFIG.Snackbar) {
+              var snackbarBg =
+                document.documentElement.getAttribute('data-theme') === 'light'
+                  ? GLOBAL_CONFIG.Snackbar.bgLight
+                  : GLOBAL_CONFIG.Snackbar.bgDark
+              var snackbarPos = GLOBAL_CONFIG.Snackbar.position
+              Snackbar.show({
+                text: '已更新最新版本',
+                backgroundColor: snackbarBg,
+                duration: 500000,
+                pos: snackbarPos,
+                actionText: '点击刷新',
+                actionTextColor: '#fff',
+                onActionClick: function (e) {
+                  location.reload()
+                },
+              })
+            } else {
+              var showBg =
+                document.documentElement.getAttribute('data-theme') === 'light'
+                  ? '#49b1f5'
+                  : '#1f1f1f'
+              var cssText = `top: 0; background: ${showBg};`
+              document.getElementById('app-refresh').style.cssText = cssText
+            }
+          }
+    }
+
+    insertDom() {
+        $(`<div class="app-refresh" id="app-refresh">
+        <div class="app-refresh-wrap">
+          <label>✨ 网站已更新最新版本 👉</label>
+          <a href="javascript:void(0)" onclick="location.reload()">点击刷新</a>
+        </div>
+      </div>`).appendTo('body');
+    }
+}
+
+
+
 btf.isJqueryLoad(
      () => {
-         new Timepiece();
+         new MyEvents();
+         new PWA();
      }
 );
